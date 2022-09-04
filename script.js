8c5edf696478952c6f89a66071ad38734ec99b97
користувач <artur.petrov.knm.2020lpnu.ua>
2022-09-04 23:10:17 +0300

    thirdv2 commit

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const byeSpeaker = {
 //3
 //This choose the names
 (function () {
-    var names = ['Artur', 'Anna', 'Mike', 'Stepan', 'Paul', 'Igor', 'Ben', 'Paula', 'Vova', 'Jimmy'];
+    var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
     for (var i = 0; i < names.length; i++) {
         var name = names[i];
         var firstLetter = name.charAt(0).toLowerCase();
